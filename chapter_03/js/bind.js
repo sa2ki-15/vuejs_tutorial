2d45628549ece6258547bfcd24b52536bb2b2999
sa2ki-15 <yamaguchikazuki733@gmail.com>
2021-01-11 00:24:34 +0900

    これから始めるVue.js実践入門:Chapter3-4まで完了

---
+++ chapter_03/js/bind.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        attrs: {
+            size: 20,
+            maxLength: 5,
+            required: true
+        },
+        text: 'こんにちは',
+        attr: 'width',
+        size: 100,
+        message:'<h3>タイトル</h3>',
+        name: '匿名'
+    }
+});
